0f5cc42ffbc8381fd0e3f02e4195f4e2b274eda8
Sarah <sarah.elfeel@gmail.com>
2025-05-09 03:15:18 +0300

    chan final

--- ChanAlgos/ChanConvexHull.java
+++ ChanAlgos/ChanConvexHull.java
@@ -119,6 +119,8 @@ public class ChanConvexHull {
             {3, -1}, {2, -2}, {1, -1}, {0, -2}, {-1, -1},
             {-2, -2}, {-3, -1}, {-4, 0}, {-3, 1}, {-2, 2}, {-1, 1}
         };
+
+        //int[][]input = {{0,0}, {1,1}, {2,2}, {3,3}, {4,4}, {5,5}, {6,6}, {7,7}, {8,8}, {9,9}, {10,10}, {11,11}};
         
         Point[] points = createPoints(input);
         List<Point> hull = chan.findConvexHull(points);
